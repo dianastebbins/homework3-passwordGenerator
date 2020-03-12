64a5b47aff8a10528867c6a87098f926553e2c83
Diana Stebbins <dianastebbins97@gmail.com>
2020-03-11 20:59:29 -0700

    generatePassword function skeletoned out

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,3 @@
-// list of alpha characters
-//     uppercase
-//     lowercase
-// list of numeric characters
-// list of special characters
-// prompt user
-//     password length
-//         check the length is between 8 and 128
-//     if they want alpha characters
-//         uppercase
-//         lowercase
-//     if they want numeric characters
-//     if they want special characters
-//         check that at least one is selected
-// build new array of potential characters based on criteria selected
-// randomly pull from new array for length specified
-//     build a password result, character by character
-// display result
 
 // for special characters, keep this in mind:
 // Code	Outputs
@@ -29,14 +11,39 @@
 // \b	backspace
 // \f	form feed
 
+// list of alpha characters
+//     uppercase
+//     lowercase
+// list of numeric characters
+// list of special characters
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  // prompt user
+  //     password length
+  //         check the length is between 8 and 128
+  //     if they want alpha characters
+  //         uppercase
+  //         lowercase
+  //     if they want numeric characters
+  //     if they want special characters
+  //         check that at least one is selected
+
+  // build new array of potential characters based on criteria selected
+
+  // randomly pull from new array for length specified
+  //     build a password result, character by character
+  return "Password";
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
+  // display result
   passwordText.value = password;
 }
 
